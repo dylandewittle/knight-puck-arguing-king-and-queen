84dd96c9e2b81ce9c71347f495f9af5d140d1870
Dylan <dylan.dewitt.le@gmail.com>
2023-05-25 19:56:16 -0500

    added get move functions, update board + check win

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from itertools import product
 class Board:    
     # represents a board in the game.
 
@@ -6,6 +7,7 @@ class Board:
         # board dimensions 
         self.rows = 9
         self.columns = 9 
+        self.king_and_queen_fight = False
 
         # reference board visualization on readme to understand new game board and how this array works.
         
@@ -14,6 +16,9 @@ class Board:
              (0, 4), (8, 4), 
              (2, 8), (6, 8), (4, 8)
             ]
+        self.encoded_board = {}
+        for i, coordinate in enumerate(self.board):
+            self.encoded_board[i] = self.encode(coordinate)
     
     """
     Design choice: What do I want the list of actions to be?
@@ -21,31 +26,103 @@ class Board:
     """
 
     # TODO: take in a new move, and update the global board.
-    def update_board(self, new_move):
-        pass
-    
+    def update_board(self, piece, new_move):
+        if piece == "wk1":
+            self.board[0] = new_move
+        elif piece == "wk2":
+            self.board[1] = new_move
+        elif piece == "wb":
+            self.board[2] = new_move
+        elif piece == "K":
+            self.board[3] = new_move 
+        elif piece == "Q":
+            self.board[4] = new_move
+        elif piece == "bk1":
+            self.board[5] = new_move
+        elif piece == "bk2":
+            self.board[6] = new_move    
+        elif piece == "bp":
+            self.board[7] = new_move
+        else:
+            print("Invalid piece entered.")      
     # TODO:
     def get_knight_moves(self, player_color):
-        pass
-    
+        if player_color == "White":
+            #moves of first white knight
+            x, y = self.board[0][0], self.board[0][1] 
+            moves = list(product([x-1, x+1],[y-2, y+2])) + list(product([x-2,x+2],[y-1,y+1]))
+            moves = [(x,y) for x,y in moves if x >= 0 and y >= 0 and x < 8 and y < 8]
+            #moves of second white knight
+            x, y = self.board[1][0], self.board[1][1] 
+            moves2 = list(product([x-1, x+1],[y-2, y+2])) + list(product([x-2,x+2],[y-1,y+1]))
+            moves2 = [(x,y) for x,y in moves2 if x >= 0 and y >= 0 and x < 8 and y < 8]
+            #total moves of both white knights, remove duplicates and coordinates king, queen, and white knights occupy
+            total_moves = set(moves + moves2)
+            total_moves = total_moves.difference((self.board[0], self.board[1], self.board[3], self.board[4]))
+            return total_moves
+        if player_color == "Black":
+            #moves of first black knight
+            x, y = self.board[5][0], self.board[5][1] 
+            moves = list(product([x-1, x+1],[y-2, y+2])) + list(product([x-2,x+2],[y-1,y+1]))
+            moves = [(x,y) for x,y in moves if x >= 0 and y >= 0 and x < 8 and y < 8]
+            #moves of second black knight
+            x, y = self.board[6][0], self.board[6][1] 
+            moves2 = list(product([x-1, x+1],[y-2, y+2])) + list(product([x-2,x+2],[y-1,y+1]))
+            moves2 = [(x,y) for x,y in moves2 if x >= 0 and y >= 0 and x < 8 and y < 8]
+            #total moves of both black knights, remove duplicates and coordinates king, queen, and black knights occupy
+            total_moves = set(moves + moves2)
+            total_moves = total_moves.difference((self.board[5], self.board[6], self.board[3], self.board[4]))
+            return total_moves
+
     def get_football_moves(self, player_color):
-        pass
+        if player_color == "White":
+            return []
+        if player_color == "Black":
+            return []
 
     def some_helper_method_to_traverse_board(self):
         pass
 
-    def get_king_and_queen_moves(self, player_color):
-        pass
+    def get_king_moves(self):
+        #if king is on edges, return only one move to left or right
+        if self.board[3][0] == 0:
+            return [(1, 4)]
+        if self.board[3][0] == 8:
+            return [(7, 4)]
+        #otherwise return moves to both left in right
+        else:
+            return [((self.board[3][0] - 1), 4), ((self.board[3][0] + 1), 4)]
+
+    def get_queen_moves(self):
+        #exact same idea as king
+        if self.board[4][0] == 0:
+            return [(1, 4)]
+        if self.board[4][0] == 8:
+            return [(7, 4)]
+        else:
+            return [((self.board[3][0] - 1), 4), ((self.board[3][0] + 1), 4)]
     
     def has_the_game_ended(self):
-        pass
+        if self.board[2][1] == 8:
+            if self.board[0] == self.board[2] or self.board[1] == self.board[2]:
+                return "White Wins"
+        if self.board[7][1] == 0:
+            if self.board[5] == self.board[7] or self.board[6] == self.board[7]:
+                return "Black Wins"
+        return "No Winner Yet"
     
     """ 
     Return a list of all the valid moves that you can take.
     """
     def obtain_all_valid_moves(self, player_color):
-        pass
-    
+        valid_moves = []
+        if player_color == "White":
+            valid_moves += self.get_king_moves() + self.get_queen_moves() + list(self.get_knight_moves("White")) + self.get_football_moves("White")
+            return valid_moves
+        if player_color == "Black":
+            valid_moves += self.get_king_moves() + self.get_queen_moves() + list(self.get_knight_moves("Black")) + self.get_football_moves("Black")
+            return valid_moves
+
     # takes a coordinate value and turns it into an integer that can represent the board.
     # hint, there is MATH INVOLVEDDD
     def encode(self, coordinate_point):
@@ -58,12 +135,9 @@ class Board:
 
     # print an encoded version of the board, well formatted in the terminal.
     def print_full_board(self): 
-        encoded_board = {}
-        for i, coordinate in enumerate(self.board):
-            encoded_board[i] = self.encode(coordinate)
         for i in range(1, 82):
-            if i in list(encoded_board.values()):
-                for key, value in encoded_board.items():
+            if i in list(self.encoded_board.values()):
+                for key, value in self.encoded_board.items():
                     if i == value:
                         if key == 0:
                             print("k", end="   ")
@@ -87,7 +161,8 @@ class Board:
                 print("\n")
 board = Board()
 board.print_full_board() 
-
+print(board.get_queen_moves())
+print(board.get_knight_moves("White"))
 
 """
 Players for the game
